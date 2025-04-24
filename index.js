b93893ef4cc002acc2eea838697a8dc009697b62
johnhesio <john_hesio@hotmail.com>
2025-04-24 12:17:45 -0300

    to_do_list_100%

--- index.js
+++ index.js
@@ -18,24 +18,30 @@ function buscarTarefas() {
 buscarTarefas();
 
 function inserirTarefas(listaDeTarefas) {
-  if (listaDeTarefas.length > 0) { 
-    lista.innerHTML = ""; 
-    listaDeTarefas.map((tarefa) => {
-      lista.innerHTML += `<li>
-            <h5>${tarefa.titulo}</h5>
-            <p>
-              ${tarefa.descricao}
-            </p>
-            <div class="actions">
-              <box-icon name="trash" size="sm"></box-icon>
-            </div>
-          </li>`;
+  if (listaDeTarefas.length > 0) {
+    lista.innerHTML = "";
+    listaDeTarefas.forEach((tarefa) => {
+      if (
+        tarefa.id &&
+        (typeof tarefa.id === "number" || typeof tarefa.id === "string")
+      ) {
+        lista.innerHTML += `<li>
+          <h5>${tarefa.titulo}</h5>
+          <p>${tarefa.descricao}</p>
+          <div class="actions">
+            <box-icon name="trash" size="sm" onclick="deletarTarefa('${tarefa.id}')"></box-icon>
+          </div>
+        </li>`;
+      } else {
+        console.error("Tarefa com ID inválido:", tarefa);
+      }
     });
   }
 }
 
 function novaTarefa() {
   event.preventDefault();
+  event.stopPropagation();
   let tarefa = {
     titulo: titulo.value,
     descricao: descricao.value,
@@ -49,7 +55,48 @@ function novaTarefa() {
   })
     .then((res) => res.json())
     .then((res) => {
-      console.log(res);
+      fecharModal();
+      buscarTarefas();
     });
-  fecharModal();
+}
+
+function deletarTarefa(id) {
+  fetch(`http://localhost:3000/tarefas/${id}`, {
+    method: "DELETE",
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      alert("Tarefa deletada com sucesso!");
+      buscarTarefas();
+    })
+    .catch((err) => console.error("Erro ao deletar tarefa:", err));
+}
+
+function pesquisarTarefaapi(descricao) {
+  fetch(`http://localhost:3000/tarefas?descricao=${encodeURIComponent(descricao)}`, {
+    method: "GET",
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.length > 0) {
+        inserirTarefas(res);
+      } else {
+        alert("Nenhuma tarefa encontrada com esse descrição.");
+        buscarTarefas();
+      }
+    })
+    .catch((err) => console.error("Erro ao buscar tarefa:", err));
+}
+
+let timeoutId; 
+
+function pesquisarTarefa() {
+  clearTimeout(timeoutId);
+  timeoutId = setTimeout(() => {
+    if (busca.value.length > 0) {
+      pesquisarTarefaapi(busca.value);
+    } else {
+      buscarTarefas();
+    }
+  }, 500);
 }
